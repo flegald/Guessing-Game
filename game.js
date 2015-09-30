fd22c5cbd28a6e041ab5ec1bdfa5e95c4d456e95
David Flegal <flegal.david@gmail.com>
2015-09-30 15:30:08 -0700

    FINAL FINAL

--- game.js
+++ game.js
@@ -1,40 +1,45 @@
 
-var buttonClicked = function(){
+
+
+var num = 0;
 
 var q1 = prompt('Was I born in Portland?').toLowerCase();
-	if (q1 ==='yes' || q1==='y'){
+	if (q1 === 'yes' || q1 === 'y'){
 		alert('Correct');
-		q1='Correct';
-	} else if (q1==='no' || q1==='n'){
+		q1Answer = 'Correct';
+		num++;
+	} else if (q1 === 'no' || q1 === 'n'){
 		alert('Incorrect');
-		q1='Incorrect';
+		q1Answer = 'Incorrect';
 	} else {
 		alert('Please type yes or no');
-		q1='Invalid';
+		q1Answer = 'Invalid';
 	}
 
 var q2 = prompt('Am I engaged?').toLowerCase();
-	if (q2 === 'yes' ||q2==='y'){
+	if (q2 === 'yes' ||q2 === 'y'){
 		alert('Correct');
-		q2='Correct';
-	} else if (q2==='no' || q2==='n'){
+		q2Answer = 'Correct';
+		num++;
+	} else if (q2 === 'no' || q2==='n'){
 		alert('Incorrect');
-		q2='Incorrect'
+		q2Answer= 'Incorrect';
 	} else {
 		alert('Please type yes or no');
-		q2='invalid';
+		q2Answer = 'Invalid';
 	}
 
 var q3 = prompt('Do I play WOW?').toLowerCase();
-	if (q3 === 'no' || q3==='n'){
+	if (q3 === 'no' || q3 === 'n'){
 		alert('Correct');
-		q3='Correct';
-	} else if (q3==='yes' || q3==='y'){
+		q3Answer = 'Correct';
+		num++;
+	} else if (q3 === 'yes' || q3 === 'y'){
 		alert('Incorrect');
-		q3='Incorrect'
+		q3Answer = 'Incorrect'
 	} else {
 		alert('Please type yes or no');
-		q3='Invalid';
+		q3Answer = 'Invalid';
 	}
 
 
@@ -42,13 +47,13 @@ console.log('q1 response ' + q1);
 console.log('q2 response ' + q2);
 console.log('q3 response ' + q3);
 
-var answers = [q1, q2, q3];
 
-alert('RESULTS: Question 1: ' + q1 + '. Question 2: ' + q2 + '. Question 3: ' + q3);
+alert('RESULTS: Question 1: ' + q1Answer + '. Question 2: ' + q2Answer + '. Question 3: ' 
+	+ q3Answer + '. You got ' + num + ' out of 3 correct.');
 
 
 
-}
+
 
 
 
